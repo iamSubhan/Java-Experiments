3221ed7ffb4a23b7a5c3a22b2a25353d2862f179
Subhan <subhanamran@gmail.com>
2022-02-01 23:24:12 +0800

    Add files via upload

---
+++ grades/Main.java
@@ -0,0 +1,26 @@
+package com.softUni;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void checkGrade(double grade) {
+        if (grade >= 2.00 && grade <= 2.99) {
+            System.out.println("Fail");
+        } else if (grade >= 3.00 && grade <= 3.49) {
+            System.out.println("Poor");
+        } else if (grade >= 3.50 && grade <= 4.49) {
+            System.out.println("Good");
+        } else if (grade >= 4.50 && grade <= 5.49) {
+            System.out.println("Very good");
+        } else if (grade >= 5.50 && grade <= 6.00) {
+            System.out.println("Excellent");
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        checkGrade(Double.parseDouble(scanner.nextLine()));
+    }
+}
